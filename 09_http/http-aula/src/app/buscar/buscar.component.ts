b23da5d37326754275ebb9a8216d69d916501c27
silvamolinaf <silvamolinaf@outlook.es>
2017-09-27 18:05:13 +0200

    https

---
+++ 09_http/http-aula/src/app/buscar/buscar.component.ts
@@ -0,0 +1,36 @@
+import { Http } from '@angular/http';
+import { Component, OnInit } from '@angular/core';
+
+const URL = "https://www.googleapis.com/books/v1/volumes?q=intitle:";
+
+@Component({
+  selector: 'app-buscar',
+  templateUrl: './buscar.component.html',
+  styleUrls: ['./buscar.component.css']
+})
+export class BuscarComponent implements OnInit {
+
+  public claveBusqueda: string;
+  public aResultados = new Array<any>();
+  constructor(private http: Http) {
+  }
+
+  ngOnInit() {
+  }
+
+  btnBuscar() {
+    this.aResultados = [];
+    console.log(this.aResultados);
+    this.http.get(URL + this.claveBusqueda).subscribe(response => console.log(response.json()),
+      // {
+      //         let data = response.json();
+      //         for (var i = 0; i < data.items.length; i++) {
+      //           let bookTitle = data.items[i].volumeInfo.title;
+      //           this.aResultados.push(bookTitle);
+      //         }
+      // },
+      error => console.error(error)
+    );
+
+  }
+}
