13f1cf25bc6bbbecbbd81dcb6ba6708ee95e9f41
silvamolinaf <silvamolinaf@outlook.es>
2017-09-28 09:38:49 +0200

    comillas

--- 09_http/http-aula/src/app/buscar/buscar.component.ts
+++ 09_http/http-aula/src/app/buscar/buscar.component.ts
@@ -1,7 +1,7 @@
 import { Http } from '@angular/http';
 import { Component, OnInit } from '@angular/core';
 
-const URL = "https://www.googleapis.com/books/v1/volumes?q=intitle:";
+const URL = 'https://www.googleapis.com/books/v1/volumes?q=intitle:';
 
 @Component({
   selector: 'app-buscar',
